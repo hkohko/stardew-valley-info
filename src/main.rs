cf69b9305e784b0978f28335f293bec491c401ec
heko <91366057+hkohko@users.noreply.github.com>
2023-12-18 12:05:32 +0700

    refactors, and tweaks on scraper

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use anyhow::Result;
+use select::document::Document;
+use select::node::Find;
+use select::predicate::{Attr, Name};
+use std::io;
 use ureq;
 use url;
-use anyhow::Result;
-use std::io;
-use select::document::Document;
-use select::predicate::{Name, Attr};
 
 fn main() {
     println!("Stardew Valley Wiki CLI");
@@ -16,7 +17,9 @@ fn main() {
         let res = make_request(sanitized_input.as_str());
         match res {
             Ok(val) => scrape_select(val.as_str(), sanitized_input),
-            Err(e) => println!("{e}\n\nItem doesn't exist/unavailable.\nTry matching the case *exactly.*\n"),
+            Err(e) => println!(
+                "{e}\n\nItem doesn't exist/unavailable.\nTry matching the case *exactly.*\n"
+            ),
         }
     }
 }
@@ -28,11 +31,40 @@ fn sanitize_input(input: &str) -> String {
 fn make_request(search_term: &str) -> Result<String> {
     let base_url = "https://stardewvalleywiki.com/";
     let parse_url = url::Url::parse(base_url).expect("Unable to parse url into a url::Url");
-    let search_for = parse_url.join(search_term).expect("Unable to join search term with base_url");
+    let search_for = parse_url
+        .join(search_term)
+        .expect("Unable to join search term with base_url");
     let agent = ureq::AgentBuilder::new().build();
     let get_resp = agent.get(search_for.as_str()).call()?;
     let page_string = get_resp.into_string()?;
     Ok(page_string)
+}
+fn display_data<'a>(
+    section: Find<'a, Attr<&'a str, &'a str>>,
+    detail: Find<'a, Attr<&'a str, &'a str>>,
+) {
+    for (s, d) in section.zip(detail) {
+        let s_text = s.text();
+        let d_text = d.text();
+        let trimmed_section = s_text.trim();
+        let trimmed_detail = d_text.trim();
+        let xp = trimmed_section.find("XP");
+        let price = trimmed_section.find("Price");
+
+        match xp {
+            Some(_) => continue,
+            None => (),
+        }
+        match price {
+            Some(_) => continue,
+            None => (),
+        }
+        match trimmed_detail.find(".mw-parser-output") {
+            Some(_) => continue,
+            None => (),
+        }
+        println!("{}: {}", s.text().trim(), d.text().trim())
+    }
 }
 fn scrape_select(page: &str, userinput: String) {
     println!("");
@@ -41,32 +73,15 @@ fn scrape_select(page: &str, userinput: String) {
     let _: Vec<()> = document
         .find(Name("table"))
         .map(|table| {
-            let section = table.find(Attr("id", "infoboxsection"));
-            let detail = table.find(Attr("id", "infoboxdetail"));
-            for (s,d) in section.zip(detail) {
-                let s_text = s.text();
-                let d_text = d.text();
-                let trimmed_section = s_text.trim();
-                let trimmmed_detail = d_text.trim();
-                let xp = trimmed_section.find("XP");
-                let price = trimmed_section.find("Price");
-                
-                match xp {
-                    Some(_) => continue,
-                    None => (),
-                }
-                match price {
-                    Some(_) => continue,
-                    None => (),
-                }
-                match trimmmed_detail.find(".mw-parser-output") {
-                    Some(_) => continue,
-                    None => {
-                        println!("{}: {}", s.text().trim(), d.text().trim());
-                        }  
-                    }
-                }
+            let _: Vec<()> = table
+                .find(Name("tr"))
+                .map(|tr| {
+                    let section = tr.find(Attr("id", "infoboxsection"));
+                    let detail = tr.find(Attr("id", "infoboxdetail"));
+                    display_data(section, detail);
+                })
+                .collect();
         })
-        .collect(); 
+        .collect();
     println!("");
 }
